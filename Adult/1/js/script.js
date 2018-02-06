ad74062617b83c1de54a014952206f299185894a
Grant-Inna <grantinna@mail.ru>
2018-02-06 18:28:37 +0300

    Edited all landings

---
+++ Adult/1/js/script.js
@@ -0,0 +1,35 @@
+function collapse () {
+    /*document.getElementById('mainCheckbox').addEventListener('click', function () { */
+    document.getElementById('firstScreen').classList.add('fadeOut'); console.info( '+' ) ;
+};
+/*
+function secondCollapse () {
+    var	checkbox = document.getElementById('hidden');
+    var btn = document.getElementById('button');
+    var finish_btn = document.getElementById('finish-button');
+    var finish_text = document.getElementById('finish-hidden');
+
+    btn.classList.add('buttons__check_collapse') ;
+    btn.classList.remove('buttons__btn_show');
+    checkbox.classList.remove('player__hidden_none');
+    checkbox.classList.add('player__hidden');
+
+    finish_btn.className += ' buttons__btn_show';
+    finish_text.className += ' finish-text';
+};
+
+function btnDisable () {
+    var btn = document.getElementById('button');
+    var check = document.getElementById('MainCheckbox');
+    if (!check.checked) {
+        console.log('!checked');
+        btn.setAttribute('disabled', '');
+    } else if (check.checked) {
+        btn.removeAttribute('disabled');
+    }
+}*/
+/*var btn = document.getElementById('button');*/
+//btn.addEventListener('click' , function () { document.getElementById('hidden').className -= ' player__hidden_none'; document.getElementById('hidden').className += ' player__hidden'; btn.className -= 'buttons__btn_show' });
+/*document.getElementById('label').addEventListener('click', collapse);
+document.getElementById('MainCheckbox').addEventListener('click', btnDisable);
+document.getElementById('button').addEventListener( 'click', secondCollapse );*/
